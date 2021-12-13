d985483fc8bd3b1393c465b6c767523053ec413a
Josip Igrec <me@allypost.net>
2021-12-13 02:30:30 +0100

    Fix cookie consent being popped up on SSR

--- store/cookieConsent.ts
+++ store/cookieConsent.ts
@@ -18,7 +18,7 @@ export const useCookieConsentStore = defineStore(
   "cookieConsent",
   {
     state: () => ({
-      consent: ConsentState.Undecided,
+      consent: ConsentState.Denied,
     }),
 
     getters: {

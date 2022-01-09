18ee12ad7caab203e0528328559d3cf4cc425660
Josip Igrec <josip@igr.ec>
2022-01-09 21:26:00 +0100

    Add mock register page

---
+++ pages/register.vue
@@ -0,0 +1,29 @@
+<template>
+  <app-max-width-container>
+    <h1>
+      <translated-text trans-key="register.header" />
+    </h1>
+  </app-max-width-container>
+</template>
+
+<script lang="ts">
+  import {
+    defineComponent,
+  } from "vue";
+  import AppMaxWidthContainer from "~/components/AppMaxWidthContainer.vue";
+  import TranslatedText from "~/components/TranslatedText.vue";
+  import useTitle from "~/composables/useTitle";
+
+  export default defineComponent({
+    name: "PageRegister",
+
+    components: {
+      TranslatedText,
+      AppMaxWidthContainer,
+    },
+
+    setup() {
+      useTitle("register.header");
+    },
+  });
+</script>

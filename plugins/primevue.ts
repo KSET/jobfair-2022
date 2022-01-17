5d801d6165982ea3eca047731133f0e10e154977
Josip Igrec <josip@igr.ec>
2022-01-17 07:38:16 +0100

    Remove unused default `Card` and `Image` PrimeVue components

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,7 +1,5 @@
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
-import Card from "primevue/card";
-import Image from "primevue/image";
 import ToastService from "primevue/toastservice";
 import {
   defineNuxtPlugin,
@@ -16,6 +14,4 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(ToastService);
 
   nuxtApp.vueApp.component("p-button", Button);
-  nuxtApp.vueApp.component("p-card", Card);
-  nuxtApp.vueApp.component("p-img", Image);
 });

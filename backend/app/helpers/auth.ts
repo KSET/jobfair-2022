297eca2e55cb780628f69a65ecb9f494bee5b963
Josip Igrec <josip@igr.ec>
2022-01-18 19:03:53 +0100

    Add very simple role comparisons to auth helpers

--- backend/app/helpers/auth.ts
+++ backend/app/helpers/auth.ts
@@ -2,6 +2,9 @@ import {
   StatusCodes,
 } from "http-status-codes";
 import {
+  SessionUser,
+} from "../types/apollo-context";
+import {
   error,
   RouteHandler,
 } from "./request";
@@ -17,6 +20,22 @@ export enum Role {
 export type AuthConfig = {
   role?: Role,
 };
+
+const roleToPriority: Record<Role, number> = {
+  [Role.Student]: 0,
+  [Role.Company]: 0,
+  [Role.AccountManager]: 5,
+  [Role.PR]: 5,
+  [Role.Admin]: Infinity,
+};
+
+export const hasAtLeastRole =
+  (
+    minimumRoleName: Role,
+    user: SessionUser,
+  ): boolean =>
+    user.roles.some((role) => roleToPriority[minimumRoleName] <= roleToPriority[role])
+;
 
 export const requireAuthMiddleware =
   <T>(config: AuthConfig): RouteHandler<T> =>

44bb1c47d02e2b553a6829018efcbdbb7fbf1f8a
Josip Igrec <josip@igr.ec>
2022-01-20 18:27:10 +0100

    Add profile auth guard

---
+++ pages/profile.vue
@@ -0,0 +1,37 @@
+<template>
+  <nuxt-nested-page
+    v-if="isLoggedIn"
+  />
+  <page-not-found
+    v-else
+  />
+</template>
+
+<script lang="ts">
+  import {
+    computed,
+    defineComponent,
+  } from "vue";
+  import {
+    useUserStore,
+  } from "~/store/user";
+  import PageNotFound from "~/components/page-not-found.vue";
+
+  export default defineComponent({
+    name: "PageProfileHandler",
+
+    components: {
+      PageNotFound,
+    },
+
+    setup() {
+      const userStore = useUserStore();
+
+      const isLoggedIn = computed(() => userStore.isLoggedIn);
+
+      return {
+        isLoggedIn,
+      };
+    },
+  });
+</script>

edacb3b64259d4df38f81bb6abfd857f02bd27fa
Josip Igrec <josip@igr.ec>
2022-01-20 10:21:52 +0100

    Fixup dump script type issue

--- backend/dump-schema.ts
+++ backend/dump-schema.ts
@@ -2,6 +2,19 @@ import {
   build,
   schemaPath,
 } from "./app/graphql/schema/helpers";
+import {
+  SessionUser,
+} from "./app/types/apollo-context";
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+    interface Request {
+      user: SessionUser | null,
+    }
+  }
+}
 
 // eslint-disable-next-line no-void
 void (async () => {

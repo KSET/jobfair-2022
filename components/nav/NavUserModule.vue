1160bd0d901b1b6ce18c1b44b0e2919172349ae8
Josip Igrec <josip@igr.ec>
2022-01-20 22:47:01 +0100

    Add proper dropdown on nav user module

--- components/nav/NavUserModule.vue
+++ components/nav/NavUserModule.vue
@@ -23,15 +23,49 @@
       <p-overlay-panel
         id="user-dropdown-panel"
         ref="op$"
+        :class="$style.overlayPanel"
         @hide="isOverlayPanelOpen = false"
         @show="isOverlayPanelOpen = true"
       >
-        <div>
-          <small v-text="user.email" />
+        <div :class="$style.overlayPanelHeaderContainer">
+          <profile-picture style="width: 3rem;" />
+
+          <div :class="$style.overlayPanelHeader">
+            <span
+              :class="$style.overlayPanelTitle"
+              v-text="user.name"
+            />
+            <span
+              :class="$style.overlayPanelSubtitle"
+              v-text="user.email"
+            />
+          </div>
         </div>
-        <v-button @click="logout">
-          Logout
-        </v-button>
+
+        <hr>
+
+        <ul :class="$style.overlayPanelItems">
+          <li>
+            <nuxt-link :to="{ name: 'profile-me' }">
+              <translated-text
+                trans-key="profile.my-profile"
+              />
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link :to="{ name: 'profile-me-settings' }">
+              <translated-text
+                trans-key="profile.settings"
+              />
+            </nuxt-link>
+          </li>
+          <li>
+            <translated-text
+              trans-key="auth.logout"
+              @click="logout"
+            />
+          </li>
+        </ul>
       </p-overlay-panel>
     </client-only>
   </div>
@@ -54,19 +88,19 @@
     watch,
   } from "vue";
   import OverlayPanel from "primevue/overlaypanel";
-  import Button from "primevue/button";
   import TranslatedText from "~/components/TranslatedText.vue";
   import useJoinNowRoute from "~/composables/useJoinNowRoute";
   import {
     useUserStore,
   } from "~/store/user";
   import IconChevronDown from "~icons/ep/arrow-down";
+  import ProfilePicture from "~/components/user/profile/profile-picture.vue";
 
   export default defineComponent({
     components: {
+      ProfilePicture,
       TranslatedText,
       POverlayPanel: OverlayPanel,
-      VButton: Button,
       IconChevronDown,
     },
 
@@ -102,6 +136,8 @@
 </script>
 
 <style lang="scss" module>
+  @use "sass:map";
+  @use "sass:color";
   @import "assets/styles/include";
 
   .container {
@@ -119,6 +155,88 @@
 
         &.dropdownIconFlipped {
           transform: rotate(180deg);
+        }
+      }
+    }
+  }
+
+  .overlayPanel {
+    $background: $fer-dark-blue;
+    $border-color: $fer-white;
+    $padding-x: 1.25rem;
+    $padding-y: .875rem;
+    $gap: $padding-y;
+
+    color: pick-visible-color($background, $fer-white, $fer-black);
+    border: 1px solid #{$border-color};
+    background: $background;
+    box-shadow: #{map.get($shadows, "shadow-4")};
+
+    &::before,
+    &::after {
+      // content: none;
+      border-bottom-color: $border-color;
+    }
+
+    :global(.p-overlaypanel-content) {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      padding: #{$padding-y} #{$padding-x};
+      gap: $gap;
+    }
+
+    .overlayPanelHeaderContainer {
+      display: flex;
+      align-items: center;
+      gap: $gap;
+    }
+
+    .overlayPanelHeader {
+      display: flex;
+      flex-direction: column;
+      gap: 2px;
+
+      .overlayPanelTitle,
+      .overlayPanelSubtitle {
+        line-height: 1.2;
+      }
+
+      .overlayPanelSubtitle {
+        font-size: .875rem;
+        opacity: .6;
+      }
+    }
+
+    hr {
+      width: calc(100% + 2 * #{$padding-x});
+      margin: 0 -#{$padding-x};
+      opacity: .3;
+    }
+
+    ul.overlayPanelItems {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+
+      li + li {
+        margin-top: .75rem;
+      }
+
+      > li {
+        text-align: right;
+
+        > * {
+          font-weight: 600;
+          line-height: 1.2rem;
+          cursor: pointer;
+          transition-property: color;
+          color: $fer-white;
+
+          &:hover {
+            transition-duration: 0s;
+            color: $fer-yellow;
+          }
         }
       }
     }

f89734903b2cffa2ea528987ff75aa29f7002229
Josip Igrec <josip@igr.ec>
2022-01-24 17:38:45 +0100

    Move validation definitions closer to export for easier scanning

--- backend/app/services/validation-service.ts
+++ backend/app/services/validation-service.ts
@@ -11,25 +11,6 @@ export const PASSWORD_LENGTH_MAX = 99 as const;
 
 const passwordValidation = z.string().min(PASSWORD_LENGTH_MIN).max(PASSWORD_LENGTH_MAX);
 
-const registerValidation = z.object({
-  email: z.string().email().max(127),
-  firstName: z.string().min(2).max(80),
-  lastName: z.string().min(2).max(80),
-  phone: z.string().min(4).max(80),
-  password: passwordValidation,
-  passwordRepeat: passwordValidation,
-});
-
-const profileValidation = registerValidation.omit({
-  password: true,
-  passwordRepeat: true,
-});
-
-const passwordUpdateValidation = z.object({
-  newPassword: passwordValidation,
-  newPasswordRepeat: passwordValidation,
-});
-
 const formatValidation =
   async <Output, Def extends ZodTypeDef = ZodTypeDef, Input = Output>(validationObject: ZodType<Output, Def, Input>, data: unknown): Promise<{ success: boolean, errors: { field: string, message: string, }[], }> => {
     const validation = await validationObject.safeParseAsync(data);
@@ -53,8 +34,27 @@ const formatValidation =
   }
 ;
 
+
+const registerValidation = z.object({
+  email: z.string().email().max(127),
+  firstName: z.string().min(2).max(80),
+  lastName: z.string().min(2).max(80),
+  phone: z.string().min(4).max(80),
+  password: passwordValidation,
+  passwordRepeat: passwordValidation,
+});
 export const RegisterValidation = <T>(user: T) => formatValidation(registerValidation, user);
 
+
+const profileValidation = registerValidation.omit({
+  password: true,
+  passwordRepeat: true,
+});
 export const ProfileValidation = <T>(user: T) => formatValidation(profileValidation, user);
 
+
+const passwordUpdateValidation = z.object({
+  newPassword: passwordValidation,
+  newPasswordRepeat: passwordValidation,
+});
 export const PasswordUpdateValidation = <T>(update: T) => formatValidation(passwordUpdateValidation, update);

21d8f2fd3245a2569a5a83487db78c87d4160a0b
Josip Igrec <josip@igr.ec>
2022-01-26 00:08:42 +0100

    Replace local font with Google's Raleway until Nuxt issue is fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,12 +39,6 @@ export default defineNuxtConfig({
   },
 
   css: [
-    "@/node_modules/@fontsource/raleway/latin.css",
-    "@/node_modules/@fontsource/raleway/latin-ext.css",
-    "@/node_modules/@fontsource/raleway/latin-400-italic.css",
-    "@/node_modules/@fontsource/raleway/latin-ext-400-italic.css",
-    "@/node_modules/@fontsource/raleway/latin-700-italic.css",
-    "@/node_modules/@fontsource/raleway/latin-ext-700-italic.css",
     "@/node_modules/normalize.css/normalize.css",
     "@/node_modules/primeflex/primeflex.css",
     "@/node_modules/primevue/resources/primevue.css",

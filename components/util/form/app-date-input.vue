607e9bac8f12803dca7edd5ed07891f032c06a94
Josip Igrec <josip@igr.ec>
2022-01-31 03:03:19 +0100

    Add basic date input element

---
+++ components/util/form/app-date-input.vue
@@ -0,0 +1,273 @@
+<template>
+  <div
+    :aria-disabled="orNull(disabled)"
+    :class="$style.container"
+  >
+    <label
+      v-if="visible.label"
+      :id="id.label"
+      :class="{
+        [$style.label]: true,
+        [$style.required]: required,
+        [$style.withColon]: !noColon,
+      }"
+      :for="id.input"
+    >
+      <slot name="label">
+        <translated-text
+          v-if="labelKey"
+          v-model="labelTranslated"
+          :trans-key="labelKey"
+        />
+        <span
+          v-else
+          v-text="label"
+        />
+      </slot>
+    </label>
+    <p-calendar
+      :id="id.input"
+      v-model="input"
+      :aria-describedby="elseNull(visible.message, id.message)"
+      :aria-errormessage="elseNull(visible.label && invalid, id.label)"
+      :aria-invalid="orNull(invalid)"
+      :aria-label="orNull(label || labelTranslated)"
+      :aria-labelledby="elseNull(visible.label, id.label)"
+      :aria-required="orNull(required)"
+      :class="{
+        [$style.input]: true,
+        [$style.invalid]: invalid,
+      }"
+      :disabled="disabled"
+      :name="name"
+      :placeholder="orNull(placeholder)"
+      :required="required"
+      :touch-u-i="true"
+    />
+    <transition name="input-message">
+      <small
+        v-if="visible.message"
+        :id="id.message"
+        :class="$style.message"
+      >
+        <slot
+          name="message"
+        />
+      </small>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+  import {
+    computed,
+    defineComponent,
+    reactive,
+    ref,
+    unref,
+  } from "vue";
+  import Calendar from "primevue/calendar";
+  import useModelWrapper from "~/composables/useModelWrapper";
+  import useReactiveSlots from "~/composables/useReactiveSlots";
+  import TranslatedText from "~/components/TranslatedText.vue";
+
+  export default defineComponent({
+    components: {
+      TranslatedText,
+      PCalendar: Calendar,
+    },
+
+    props: {
+      modelValue: {
+        required: false,
+        type: [ String, Date ],
+        default: () => "",
+      },
+
+      name: {
+        required: true,
+        type: String,
+      },
+
+      required: {
+        required: false,
+        type: Boolean,
+        default: () => false,
+      },
+
+      noColon: {
+        required: false,
+        type: Boolean,
+        default: () => false,
+      },
+
+      label: {
+        required: false,
+        type: String,
+        default: () => "",
+      },
+
+      labelKey: {
+        required: false,
+        type: String,
+        default: () => "",
+      },
+
+      invalid: {
+        required: false,
+        type: Boolean,
+        default: () => false,
+      },
+
+      disabled: {
+        required: false,
+        type: Boolean,
+        default: () => false,
+      },
+
+      placeholder: {
+        required: false,
+        type: String,
+        default: () => "",
+      },
+    },
+
+    emits: [ "update:modelValue" ],
+
+    setup(props, { emit }) {
+      const uniqueId = Math.random().toString(36).substring(2);
+      const slotExists = useReactiveSlots("message", "label");
+
+      const input = useModelWrapper(props, emit)("modelValue");
+
+      const inputId = computed(() => `input-${ uniqueId }-${ props.name }`);
+
+      function elseNull<T, C>(check: C, value: T) {
+        return check ? value : null;
+      }
+
+      function orNull<T>(value: T) {
+        return elseNull(value, value);
+      }
+
+      return {
+        input,
+        id: reactive({
+          input: inputId,
+          message: computed(() => `${ unref(inputId) }--message`),
+          label: computed(() => `${ unref(inputId) }--label`),
+        }),
+        visible: reactive({
+          label: computed(() => Boolean(props.label || props.labelKey || slotExists.label.value)),
+          message: computed(() => slotExists.message.value),
+        }),
+        labelTranslated: ref(""),
+        elseNull,
+        orNull,
+      };
+    },
+  });
+</script>
+
+<style lang="scss" module>
+  @use "sass:color";
+  @import "assets/styles/include";
+
+  .container {
+    position: relative;
+    display: inline-block;
+
+    &:global([aria-disabled="true"]) {
+      opacity: .5;
+    }
+
+    .label {
+      font-weight: bold;
+      display: block;
+      margin-bottom: .75rem;
+      cursor: pointer;
+      color: #{color.adjust($fer-black, $alpha: -.2)};
+
+      &.withColon {
+
+        &::after {
+          content: ":";
+        }
+      }
+
+      &.required {
+
+        &::before {
+          font-weight: bold;
+          content: "*";
+          color: $fer-error;
+        }
+      }
+    }
+
+    .input {
+      font-size: 1rem;
+      width: 100%;
+      transition-property: outline-color, border-color;
+      color: $fer-black;
+      border: 1px solid #{color.adjust($fer-black, $alpha: -.6)};
+      border-radius: 4px;
+      outline: transparent solid 2px;
+      appearance: none;
+
+      :global(.p-dropdown-label) {
+        padding: .625rem;
+      }
+
+      &:focus {
+        border-color: #{$fer-yellow};
+        outline-color: #{$fer-yellow};
+      }
+
+      &.invalid {
+        border-color: #{$fer-error};
+
+        &:focus {
+          outline-color: #{$fer-error};
+        }
+      }
+    }
+
+    $message-margin: .25rem;
+
+    .message {
+      font-size: 80%;
+      display: block;
+      margin-top: $message-margin;
+      transition-property: opacity, transform, color;
+      opacity: .75;
+      color: $fer-black;
+    }
+
+    .input.invalid + .message {
+      opacity: 1;
+      color: $fer-error;
+    }
+
+    :global {
+
+      .input-message {
+
+        &-enter-active,
+        &-leave-active {
+          transform: translateY(0);
+        }
+
+        &-enter-active {
+          transition-duration: .1s;
+        }
+
+        &-enter-from,
+        &-leave-to {
+          transform: translateY(calc(-1 * #{$message-margin}));
+          opacity: 0;
+        }
+      }
+    }
+  }
+</style>

969afce0aef55d8c5a203fdf90ba1d5fd2c9cb1a
Josip Igrec <josip@igr.ec>
2022-02-10 08:01:21 +0100

    Add image route

---
+++ backend/app/routes/i/[uid].ts
@@ -0,0 +1,94 @@
+import {
+  StatusCodes,
+} from "http-status-codes";
+import {
+  Router,
+} from "../../helpers/route";
+import {
+  prisma,
+} from "../../providers/prisma";
+import {
+  RouteHandler,
+} from "../../helpers/request";
+import {
+  pipeFileInto,
+} from "../../helpers/minio";
+
+const router = new Router();
+
+const serveImage =
+  (size: "full" | "thumb" | "original", uid: string): RouteHandler<unknown> =>
+    async (req, res) => {
+      const fileObj = await prisma.image.findUnique({
+        where: {
+          uid,
+        },
+        select: {
+          name: true,
+          [size]: {
+            select: {
+              minioKey: true,
+              mimeType: true,
+              etag: true,
+            },
+          },
+        },
+      });
+
+      if (!fileObj) {
+        return res.sendStatus(StatusCodes.NOT_FOUND);
+      }
+
+      /* eslint-disable @typescript-eslint/ban-ts-comment */
+      const file = {
+        // @ts-ignore
+        name: fileObj.name as string,
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        mimeType: fileObj[size].mimeType as string,
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        minioKey: fileObj[size].minioKey as string,
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        etag: fileObj[size].etag as string,
+      };
+      /* eslint-enable @typescript-eslint/ban-ts-comment */
+
+      const providedEtag = req.header("if-none-match");
+
+      if (providedEtag && providedEtag === file.etag) {
+        return res.sendStatus(StatusCodes.NOT_MODIFIED);
+      }
+
+      res
+        .header("content-type", file.mimeType)
+        .header("etag", file.etag)
+        .header("cache-control", "max-age=31536000, public, immutable")
+      ;
+
+      await pipeFileInto(file.minioKey, res);
+
+      return res.end();
+    }
+;
+
+router.getRaw("/full", (req, res) => {
+  const { uid } = req.params as Record<string, string>;
+
+  return serveImage("full", uid)(req, res, () => void 0);
+});
+
+router.getRaw("/thumb", (req, res) => {
+  const { uid } = req.params as Record<string, string>;
+
+  return serveImage("thumb", uid)(req, res, () => void 0);
+});
+
+router.getRaw("/original", (req, res) => {
+  const { uid } = req.params as Record<string, string>;
+
+  return serveImage("original", uid)(req, res, () => void 0);
+});
+
+export default router;

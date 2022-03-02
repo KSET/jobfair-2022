1b64330481409008a405ac03703e1fdfc3562933
Josip Igrec <josip@igr.ec>
2022-03-02 10:29:01 +0100

    Add simple statistics to admin applications overview

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <app-max-width-container>
+  <app-max-width-container :class="$style.container">
     <h1>
       Admin
     </h1>
@@ -133,8 +133,8 @@
         <edit-season
           :season="season"
           :uid="season.uid"
-          @save="refreshSeasons"
           @delete="refreshSeasons"
+          @save="refreshSeasons"
         />
       </fieldset>
     </div>
@@ -190,6 +190,61 @@
 
     <div>
       <h2>Prijave ({{ companyApplications.length }})</h2>
+      <div :class="$style.statistics">
+        <div>
+          <strong>Industrije</strong>
+          <dl>
+            <dd
+              v-for="(count, name) in statistics.company.byIndustry"
+              :key="name"
+            >
+              <span v-text="name" />
+              <data :value="count" v-text="count" />
+            </dd>
+          </dl>
+        </div>
+
+        <div>
+          <strong>Štandovi</strong>
+          <dl>
+            <dd
+              v-for="(count, name) in statistics.booths"
+              :key="name"
+            >
+              <span v-text="booths[name]" />
+              <data :value="count" v-text="count" />
+            </dd>
+          </dl>
+        </div>
+
+        <div>
+          <strong>Talkovi</strong>
+          <dl>
+            <dd
+              v-for="(count, name) in statistics.talks"
+              :key="name"
+            >
+              <span v-text="name" />
+              <data :value="count" v-text="count" />
+            </dd>
+          </dl>
+        </div>
+
+        <div>
+          <strong>Workshopovi</strong>
+          <data :value="statistics.workshops" v-text="statistics.workshops" />
+        </div>
+
+        <div>
+          <strong>Kokteli</strong>
+          <data :value="statistics.cocktails" v-text="statistics.cocktails" />
+        </div>
+
+        <div>
+          <strong>Paneli</strong>
+          <data :value="statistics.panels" v-text="statistics.panels" />
+        </div>
+      </div>
       <ul>
         <li
           v-for="application in companyApplications"
@@ -203,7 +258,6 @@
           />
           <strong v-text="application.forCompany.brandName" />
           <a
-            class="ml-2"
             :href="$router.resolve({
               name: 'admin-company-application-season-company-edit',
               params: {
@@ -211,6 +265,7 @@
                 company: application.forCompany.uid,
               },
             }).href"
+            class="ml-2"
           >Edit</a>
           <ul>
             <li v-if="application.booth" class="mt-3 mb-2">
@@ -257,6 +312,7 @@
     defineComponent,
     reactive,
     ref,
+    unref,
   } from "vue";
   import {
     gql,
@@ -335,7 +391,52 @@
 
       const companies = ref(res?.companies);
       const booths = ref(Object.fromEntries((res?.booths || []).map((b) => [ b.key, b.name ])));
+
+      const statistics = {
+        company: {
+          byIndustry: Object.fromEntries(industriesStore.industries.map((industry) => [ industry, 0 ])),
+        },
+        booths: Object.fromEntries((res?.booths || []).filter((b) => b.key).map((b) => [ b.key, 0 ])) as Record<string, number>,
+        talks: Object.fromEntries((res?.talkCategories || []).map((c) => [ c.name, 0 ])),
+        workshops: 0,
+        panels: 0,
+        cocktails: 0,
+      };
+      for (const application of unref(res?.companyApplications) ?? []) {
+        const {
+          booth,
+          talk,
+          workshop,
+          wantsPanel,
+          wantsCocktail,
+          forCompany: company,
+        } = application;
+
+        statistics.company.byIndustry[company!.industry!.name] += 1;
+
+        if (booth) {
+          statistics.booths[booth] += 1;
+        }
+
+        if (talk) {
+          statistics.talks[talk.category.name] += 1;
+        }
+
+        if (workshop) {
+          statistics.workshops += 1;
+        }
+
+        if (wantsCocktail) {
+          statistics.cocktails += 1;
+        }
+
+        if (wantsPanel) {
+          statistics.panels += 1;
+        }
+      }
+
       return {
+        statistics,
         industries,
         talkCategories,
         booths,
@@ -415,3 +516,46 @@
     },
   });
 </script>
+
+<style lang="scss" module>
+  @import "assets/styles/include";
+
+  .container {
+
+    .statistics {
+      display: flex;
+      flex-direction: column;
+      width: fit-content;
+      margin-left: 1.5rem;
+      padding-left: .875rem;
+      border-left: 2px solid #{$fer-yellow};
+      gap: 1rem;
+
+      > div {
+
+        > strong {
+
+          &::after {
+            content: ": ";
+          }
+        }
+
+        > dl {
+          margin: 0 1rem;
+
+          dd {
+            margin: 0;
+
+            > span {
+              font-style: italic;
+
+              &::after {
+                content: ": ";
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+</style>

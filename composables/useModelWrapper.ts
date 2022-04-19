b7db982c52af6620f74b068592e8a3824e27ec81
Josip Igrec <josip@igr.ec>
2022-04-19 04:12:49 +0200

    Make model value remember prop name in type

--- composables/useModelWrapper.ts
+++ composables/useModelWrapper.ts
@@ -1,11 +1,17 @@
 import {
   computed,
 } from "vue";
+import type {
+  WritableComputedRef as ComputedRef,
+} from "vue";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type WritableComputedRef<Name, T> = ComputedRef<T>;
 
 export default <Props extends Record<string, unknown>, Event extends string, Param>(props: Props, emit: (event: Event, ...params: Param[]) => void) =>
-  <Prop extends keyof Props>(prop: Prop) =>
+  <Prop extends keyof Props, ModelType = Props[Prop]>(prop: Prop) =>
     computed({
       get: () => props[prop],
       set: (value) => emit(`update:${ prop.toString() }` as Event, value as Param),
-    })
+    }) as WritableComputedRef<Prop, ModelType>
 ;

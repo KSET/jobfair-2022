9f986c5dcee134ae738dd40e079bc6734d5b3446
Pavle Ergović <80279109+pavleerg@users.noreply.github.com>
2023-02-23 02:28:45 +0100

    Change static Cookie policy to translation (#11)

--- components/nav/CookieConsent.vue
+++ components/nav/CookieConsent.vue
@@ -60,132 +60,14 @@
           v-model:visible="dialog"
           :style="{ maxWidth: '700px' }"
           dismissable-mask
-          header="Pravila o kolačićima za Job Fair Meetup"
+          :header="headerTranslation"
           maximizable
           modal
           position="bottom"
         >
-          <p>
-            This is the Cookie Policy for Job Fair, accessible from https://jobfair.fer.unizg.hr/
-          </p>
-
-          <div>
-            <p>
-              <strong>What Are Cookies</strong>
-            </p>
-
-            <p>
-              As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what
-              information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break'
-              certain elements of the site's functionality.
-            </p>
-            <p>
-              For more general information on cookies, please read <a href="https://www.privacypolicyonline.com/what-are-cookies/" target="_blank" rel="noopener noreferrer">"What Are Cookies"</a>.
-            </p>
-          </div>
-
-          <div>
-            <p>
-              <strong>How We Use Cookies</strong>
-            </p>
-
-            <p>
-              We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and
-              features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.
-            </p>
-          </div>
-
-          <div>
-            <p>
-              <strong>Disabling Cookies</strong>
-            </p>
-
-            <p>
-              You can prevent the setting of cookies by adjusting the settings on your browser (see your browser official help page for how to do this). Be aware that disabling cookies will affect the functionality
-              of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of this site. Therefore it is recommended that you do not
-              disable cookies.
-            </p>
-          </div>
-
-          <div>
-            <p>
-              <strong>The Cookies We Set</strong>
-            </p>
-
-            <ul>
-              <li>
-                Account related cookies
-                <br>
-                If you create an account with us then we will use cookies for the management of the signup process and general administration. These cookies will usually be deleted when you log out however in some
-                cases they may remain afterwards to remember your site preferences when logged out.
-              </li>
-
-              <li>
-                Login related cookies
-                <br>
-                We use cookies when you are logged in so that we can remember this fact. This prevents you from having to log in every single time you visit a new page. These cookies are typically removed or cleared
-                when you log out to ensure that you can only access restricted features and areas when logged in.
-              </li>
-
-              <li>
-                Forms related cookies
-                <br>
-                When you submit data through a form such as those found on registration pages, cookies may be set to remember your user details for future correspondence.
-              </li>
-
-              <li>
-                Site preferences cookies
-                <br>
-                In order to provide you with a great experience on this site we provide the functionality to set your preferences for how this site runs when you use it. In order to remember your preferences we need
-                to set cookies so that this information can be called whenever you interact with a page that is affected by your preferences.
-              </li>
-            </ul>
-
-            <p />
-          </div>
-
-          <div>
-            <p>
-              <strong>Third Party Cookies</strong>
-            </p>
-
-            <p>
-              In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.
-            </p>
-
-            <ul>
-              <li>
-                This site uses Google Analytics which is one of the most widespread and trusted analytics solutions on the web for helping us to understand how you use the site and ways that we can improve your
-                experience. These cookies may track things such as how long you spend on the site and the pages that you visit so we can continue to produce engaging content.
-                <br>
-                For more information on Google Analytics cookies, see the official Google Analytics page.
-              </li>
-
-              <li>
-                From time to time we test new features and make subtle changes to the way that the site is delivered. When we are still testing new features these cookies may be used to ensure that you receive a
-                consistent experience whilst on the site whilst ensuring we understand which optimisations our users appreciate the most.
-              </li>
-            </ul>
-
-            <p />
-          </div>
-
-          <div>
-            <p>
-              <strong>More Information</strong>
-            </p>
-
-            <p>
-              Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it
-              does interact with one of the features you use on our site.
-            </p>
-          </div>
-
-          <div>
-            <p>
-              However if you are still looking for more information then you can contact us through our email: <a href="mailto:jobfair@fer.hr" target="_blank" rel="noopener noreferrer">jobfair@fer.hr</a>.
-            </p>
-          </div>
+          <translated-text
+            trans-key="footer.legal.cookiePolicy.text"
+          />
         </PDialog>
       </LazyClientOnly>
     </div>
@@ -209,6 +91,9 @@
   // noinspection TypeScriptCheckImport
   import IconClose from "~icons/ep/close-bold";
   import TranslatedText from "~/components/TranslatedText.vue";
+  import {
+    useTranslationsStore,
+  } from "~~/store/translations";
 
   export default defineComponent({
     components: {
@@ -220,13 +105,14 @@
 
     setup() {
       const cookieConsentStore = useCookieConsentStore();
-
+      const translationStore = useTranslationsStore();
       const dialog = ref(false);
 
       return {
         acceptConsent: () => cookieConsentStore.acceptConsent(),
         denyConsent: () => cookieConsentStore.denyConsent(),
         showConsent: computed(() => cookieConsentStore.showConsent),
+        headerTranslation: computed(() => translationStore.translation("footer.legal.cookiePolicy.header")),
         img: {
           iconCookie: IconCookie,
         },

81599593b267676f9a1358ef88247ebecc574321
Josip Igrec <josip.igrec@kset.org>
2023-03-02 15:34:34 +0100

    Update events service to return true/false on log

--- backend/app/services/events-service.ts
+++ backend/app/services/events-service.ts
@@ -1,31 +1,33 @@
 import {
-  EventLog,
-} from "@generated/type-graphql";
-import {
   prisma,
 } from "../providers/prisma";
 
 export class EventsService {
-  public static logEvent(name: string, userId?: number | null, payload?: unknown): Promise<EventLog | null> {
-    try {
-      const data = {
-        name,
-        data: payload !== undefined ? JSON.stringify(payload) : payload,
+  public static logEvent(name: string, userId?: number | null, payload?: unknown): Promise<boolean> {
+    const data = {
+      name,
+      data: payload !== undefined ? JSON.stringify(payload) : payload,
+    };
+
+    if (userId) {
+      (data as Record<string, unknown>).user = {
+        connect: {
+          id: userId,
+        },
       };
-
-      if (userId) {
-        (data as Record<string, unknown>).user = {
-          connect: {
-            id: userId,
-          },
-        };
-      }
-
-      return prisma.eventLog.create({
-        data,
-      }).then((x) => x);
-    } catch {
-      return Promise.resolve(null);
     }
+
+    return (
+      prisma
+        .eventLog
+        .create({
+          data,
+          select: {
+            id: true,
+          },
+        })
+        .then(() => true)
+        .catch(() => false)
+    );
   }
 }

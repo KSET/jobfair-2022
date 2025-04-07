bd2fc98dab4f38be2a788e075a8895d2b1aaf1b4
Pavle Ergović <80279109+pavleerg@users.noreply.github.com>
2025-04-08 01:11:07 +0200

    Freat/re re add cv (#18)
    * change company aplication to be a table
    
    * rea-added cv, made privacy policy popup on form GDPR
    
    * returned some files to old form

--- store/pages.ts
+++ store/pages.ts
@@ -125,7 +125,7 @@ export const usePagesStore = defineStore(
           {
             name: "profile.cv",
             to: { name: "profile-me-cv" },
-            if: () => !userStore.hasCompany && false,
+            if: () => !userStore.hasCompany,
           },
         ] as const);
       },

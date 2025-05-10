33dc3529961723fc1ea65f7c37be12de4a183469
Dino Plecko <dinoplecko@yahoo.com>
2025-05-11 00:38:50 +0200

    add internships Query

--- backend/app/graphql/resolvers/companyApplicationInternship.ts
+++ backend/app/graphql/resolvers/companyApplicationInternship.ts
@@ -1,16 +1,74 @@
-import { ApplicationInternship } from "@generated/type-graphql";
+import { ApplicationInternship, FindManyApplicationInternshipArgs } from "@generated/type-graphql";
 import {
+  Args,
+  Ctx,
   Field,
+  Info,
   InputType,
+  Query,
   Resolver,
 } from "type-graphql";
-import { transformSelectFor } from "../helpers/resolver";
+import { toSelect, transformSelectFor } from "../helpers/resolver";
+import { GraphQLResolveInfo } from "graphql";
+import { Context } from "../../types/apollo-context";
 
 
 @Resolver(() => ApplicationInternship)
 export class CompanyApplicationInternshipFieldResolver {
 }
 export const transformSelect = transformSelectFor<CompanyApplicationInternshipFieldResolver>({});
+
+
+@Resolver(() => ApplicationInternship)
+export class CompanyApplicationInternshipFindResolver {
+  @Query(() => [ ApplicationInternship ])
+  internships(
+  @Ctx() ctx: Context,
+    @Info() info: GraphQLResolveInfo,
+    @Args() args: FindManyApplicationInternshipArgs,
+  ) {
+    const now = new Date();
+
+    return ctx.prisma.applicationInternship.findMany({
+          ...args,
+          cursor: undefined,
+          where: {
+            ...(
+              args.where
+                ? args.where
+                : { 
+                  forApplication: {
+                  forSeason: {
+                    startsAt: {
+                      lte: now,
+                    },
+                    endsAt: {
+                      gte: now,
+                    },
+                  }
+                }
+              }
+            )
+            // ...(
+            //   args.where
+            //     ? args.where
+            //     : {
+            //       forSeason: {
+            //         startsAt: {
+            //           lte: now,
+            //         },
+            //         endsAt: {
+            //           gte: now,
+            //         },
+            //       },
+            //     }
+            // ),
+          },
+          select: toSelect(info, transformSelect),
+    })
+  }
+}
+
 
 @InputType()
 export class InternshipCreateInput {
@@ -29,4 +87,6 @@ export class InternshipCreateInput {
   @Field()
     duration: string = "";
 }
+
+
 
